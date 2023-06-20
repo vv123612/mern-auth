599114ddf7e0d6c2adf0bdbe36f16e417a7bff97
vitaly <v123612@gmail.com>
2023-06-20 19:30:56 +0300

    add changes for docker

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,9 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
+    // const conn = await mongoose.connect(process.env.MONGO_URI);
+    const conn = await mongoose.connect("mongodb://root:example@mongo:27017/mernauth?authSource=admin");
+    
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`Error: ${error.message}`);
